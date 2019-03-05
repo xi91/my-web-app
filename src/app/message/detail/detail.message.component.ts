cf9891874a8eb8650e38c58823339e51c0445448
xi91 <xi_www@163.com>
2019-03-05 23:54:55 +0800

    update message 1

---
+++ src/app/message/detail/detail.message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-detail',
+  templateUrl: './detail.message.component.html',
+  styleUrls: ['./detail.message.component.css']
+})
+export class DetailMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/message/update/update.message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-update',
+  templateUrl: './update.message.component.html',
+  styleUrls: ['./update.message.component.css']
+})
+export class UpdateMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
